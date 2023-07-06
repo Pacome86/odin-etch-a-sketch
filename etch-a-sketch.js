41f13b26f533ac264a749be9cb09f0ceb86a45f1
Pacôme MARTIN <pacome.martin@hotmail.fr>
2023-07-06 18:38:37 +0200

    Set up a 'hover' effect to change divs color when mouseover

--- etch-a-sketch.js
+++ etch-a-sketch.js
@@ -1,3 +1,5 @@
+// Create a 16x16 grid with square div
+
 const container = document.querySelector('#container');
 
 for (let i = 0; i < 256; i++) {
@@ -5,4 +7,15 @@ for (let i = 0; i < 256; i++) {
     div.classList.add('square');
     container.appendChild(div);
 }
-console.log(container);
+// console.log(container);
+
+// Set-up a "hover" effect so that the grid divs change color 
+// when the mouse passes over them 
+const squares = Array.from(document.querySelectorAll('.square'));
+console.log(squares);
+squares.forEach(div => div.addEventListener('mouseover', changeColor));
+
+function changeColor(e) {
+    // console.log(e);
+    e.target.style.backgroundColor = 'black';
+}
